e8837521138294270009eea04d4d8f8e3f0aa3fe
Lucas Marcondes <lucas.marcondes@wallawalla.edu>
2018-04-25 15:55:19 -0700

    Final commit for HW 3

--- hw03/IsThereArray.cpp
+++ hw03/IsThereArray.cpp
@@ -3,9 +3,9 @@
  * hw02: Implement IsThere() for Array and Linked implementations
  * 
  * File Name:   IsThereArray.cpp
- * Name:        ?
+ * Name:        Lucas Marcondes
  * Course:      CPTR 242
- * Date:        ?
+ * Date:        4/25/18
  * Time Taken:  ?
  * 
  */
@@ -18,7 +18,28 @@
 // 	Postcondition:	Function value = there exist an item in the list whose key is the same as item's.	
 bool SortedArray::IsThere(ItemType item) const {
     
-        // TODO Implement the Array version of IsThere() using binary search.
-    
-    return false;
+	int first = GetLength() - 1;
+	int last = 0;
+	bool moreToSearch = last <= first;
+	int mid;
+
+	while (moreToSearch) {
+		mid = (first + last) / 2;
+		switch (item.ComparedTo(info[mid])) 
+		{
+		case GREATER:
+			last = mid + 1;
+			moreToSearch = last <= first;
+			break;
+		case LESS:
+			first = last - 1;
+			moreToSearch = last <= first;
+			break;
+		case EQUAL:
+			return true;
+			break;
+		}
+	}
+	return false;
+
 }
--- hw03/IsThereLinked.cpp
+++ hw03/IsThereLinked.cpp
@@ -3,9 +3,9 @@
  * hw02: Implement IsThere() for Array and Linked implementations
  * 
  * File Name:   IsThereLinked.cpp
- * Name:        ?
+ * Name:        Lucas Marcondes
  * Course:      CPTR 242
- * Date:        ?
+ * Date:        4/25/18
  * Time Taken:  ?
  * 
  */
@@ -17,8 +17,23 @@
 //  Precondition:	List has been initialized.
 // 	Postcondition:	Function value = there exist an item in the list whose key is the same as item's.	
 bool SortedLinked::IsThere(ItemType item) const {
-    
-    // TODO Implement the Linked version of IsThere() using an early exit if not found.
-    
-    return false;
-}
+
+	NodeType* location;
+	location = listData;
+
+	for (int i = 0; i < GetLength(); i++)
+	{
+		switch (item.ComparedTo(location->info))
+		{
+		case GREATER:
+			location = location->next;
+			break;
+		case EQUAL:
+			return true;
+		case LESS:
+			return false;
+			break;
+		}
+	}
+		return false;
+	}
