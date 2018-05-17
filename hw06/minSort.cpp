d55f1ba86fbd1470d9205820b77aa7a353470522
Preston Carman <ecarm002@ucr.edu>
2018-05-17 16:20:35 +0000

    Adding recursion homework

---
+++ hw06/minSort.cpp
@@ -0,0 +1,24 @@
+/****************************************************************************
+ *
+ * Howemork.06: Implement a two recursive functions on a linked unsorted list.
+ *
+ * File Name:  minSort.cpp
+ * Name:       ?
+ * Course:     CPTR 242
+ * Date:       ?
+ * Time Taken: ?
+ *
+ */
+#include "ItemType.h"
+#include "unsorted.h"
+
+using namespace std;
+
+NodeType* MinLoc(NodeType* list, NodeType*& minPtr) {
+    // TODO Add recursive MinLoc.
+    return NULL;
+}
+
+void Sort(NodeType* list) {
+    // TODO Add recursive sort method that uses MinLoc.
+}
