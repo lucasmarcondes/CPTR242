3f0c9c58a30d792e0a58adb2c045b1fba8b48a56
Preston Carman <ecarm002@ucr.edu>
2018-06-01 03:24:40 +0000

    Adding alternate Project 1.

---
+++ proj01_regrade/testLists.h
@@ -0,0 +1,14 @@
+#include "ItemType.h"
+#include "ListType.h"
+
+using namespace std;
+
+// Provided to students to test.
+void mergeLists(ListType list1, ListType list2, ListType& result);
+
+void splitLists(ListType list, ItemType item, ListType& list1, ListType& list2);
+
+// Written as part of assignment.
+bool testMergeLists(ListType list1, ListType list2);
+
+bool testSplitLists(ListType list, ItemType item);
